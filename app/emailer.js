e6b786dbcffa31273ba086ec3e12471b1df4b696
tim <timothy.potts@colorado.edu>
2019-12-18 19:29:58 -0700

    got email applicaton to work

--- app/emailer.js
+++ app/emailer.js
@@ -1,1 +1,26 @@
 const nodemailer = require("nodemailer");
+
+let transport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "craigfinderalert@gmail.com",
+    pass: "b00g3yman"
+  }
+});
+
+const message = {
+  from: "craigfinderalert@gmail.com", // Sender address
+  to: "gunhodarro@gmail.com", // List of recipients
+  subject: `This is a test email`, // Subject line
+  text: "Here is a link to the listing" // Plain text body
+};
+
+transport.sendMail(message, function(err, info) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Email sent: " + info);
+  }
+});
