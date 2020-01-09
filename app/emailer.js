cca76755fd02093fe25bafa958d55742de2d0a91
tim <timothy.potts@colorado.edu>
2020-01-08 18:53:52 -0700

    starting to build the mailer function as a model

--- app/emailer.js
+++ app/emailer.js
@@ -1,26 +1,33 @@
 const nodemailer = require("nodemailer");
 
-let transport = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "craigfinderalert@gmail.com",
-    pass: "b00g3yman"
-  }
-});
+function mailer(user) {
+  var subject = `We found a ${user.listing} in your area!`;
+  var emailBody = `We have found a listing that matches your parameters!:
+  ${listingLink}`;
+  var accountEmail = user.email;
 
-const message = {
-  from: "craigfinderalert@gmail.com", // Sender address
-  to: accountEmail, // List of recipients
-  subject: subject, // Subject line
-  text: emailBody // Plain text body
-};
+  let transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "craigfinderalert@gmail.com",
+      pass: "b00g3yman"
+    }
+  });
 
-transport.sendMail(message, function(err, info) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Email sent: " + info);
-  }
-});
+  const message = {
+    from: "craigfinderalert@gmail.com", // Sender address
+    to: accountEmail, // List of recipients
+    subject: subject, // Subject line
+    text: emailBody // Plain text body
+  };
+
+  transport.sendMail(message, function(err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email sent: " + info);
+    }
+  });
+}
