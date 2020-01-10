f7ad92f5fcdc7a646a9ff7164ed879b3d6548d0f
tim <timothy.potts@colorado.edu>
2020-01-09 17:04:18 -0700

    config shit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var db = require("./models");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
