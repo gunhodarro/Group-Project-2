e41c4067e4c1d063df33e366e9a8c2b327c2a181
matthew juretic <gunhodarro@gmail.com>
2020-01-10 18:32:01 -0700

    updated location api

--- location.js
+++ location.js
@@ -1,15 +1,16 @@
-var x=document.getElementById("demo");
-function getLocation()
- {
-if (navigator.geolocation)
-{
-navigator.geolocation.getCurrentPosition(showPosition);
-}
-else{x.innerHTML="Geolocation is not supported by this browser.";}
-}
-function showPosition(position)
-{
-x.innerHTML="Latitude: " + position.coords.latitude + 
-"<br>Longitude: " + position.coords.longitude;  
-}
-getLocation()
+$('#location-button').click(function(){
+
+    if(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(function(position){
+            console.log(position);
+            $.get( "http://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBjYUEgKyxA_Nh3ulDtC3vl6dV7PbT_Xh8&latlng="+ position.coords.latitude + "," + position.coords.longitude + "&sensor=false", function(data) {
+                console.log(data);
+            })
+
+            var img = new Image();
+            img.src = "https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBjYUEgKyxA_Nh3ulDtC3vl6dV7PbT_Xh8&center=" + position.coords.latitude + "," + position.coords.longitude + "&zoom=13&size=800x400&sensor=false";
+            $('#output').html(img);
+        });
+    else 
+    console.log("geolocation is not supported");
+});
